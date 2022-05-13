c03c4ee76a0d0918b4faeb23ba542b5320c80c01
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-05-13 16:46:05 +0200

    Adds app extension

---
+++ ext/app/src/index.ts
@@ -0,0 +1,17 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the IO-App extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'IO-App:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension IO-App is activated!');
+  }
+};
+
+export default plugin;
