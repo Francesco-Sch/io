a285159eda0cf0a1a88d04eb2cbc3f4674fdc07f
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-05-19 10:34:22 +0200

    Merge theme and app extension for easier development

--- ext/app/src/index.ts
+++ ext/app/src/index.ts
@@ -3,6 +3,32 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/*
+* Theme
+*/
+const theme: JupyterFrontEndPlugin<void> = {
+  id: 'io:theme',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('IO-Theme is activated.');
+    const style = 'IO-App/index.css'
+
+    manager.register({
+      name: 'IO-Theme',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    })
+  }
+
+}
+
+/*
+* Icon Set of IBM Carbon Design System.
+*/ 
 export * from './icons/icons';
 
 /**
@@ -12,8 +38,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'IO-App:plugin',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension IO-App is activated!');
+    console.log('IO-App is activated.');
+
+    // Activate IO-Theme on extension start
+    app.commands.execute('apputils:change-theme', {
+      theme: 'IO-Theme'
+    })
   }
 };
 
-export default plugin;
+/*
+* Export all plugins
+*/
+const plugins: Array<JupyterFrontEndPlugin<any>> = [
+  theme,
+  plugin
+]
+
+export default plugins;
