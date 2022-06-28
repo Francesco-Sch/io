30b0f4da20d20f9442ab9743aced84aab4658cdf
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-06-28 21:41:07 +0200

    Website: Adds header component

--- website/src/components/Header.svelte
+++ website/src/components/Header.svelte
@@ -1,9 +1,56 @@
 <script lang="ts">
-
+const navigation:object[] = [
+    {
+        name: 'Introduction',
+        link: '#introduction'
+    },
+    {
+        name: 'The Framework',
+        link: '#the-framework'
+    },
+    {
+        name: 'Notebooks',
+        link: '#notebooks'
+    },
+    {
+        name: 'Documentation',
+        link: '#documentation'
+    }
+]   
 </script>
 
 <header>
-    <h1>This is a cool website.</h1>
+    <img src="./logo.svg" alt="Logo of the IO framework" class="logo">
+    <nav>
+        <ul>
+            {#each navigation as link}
+            <li>
+                <a href="{link.link}">{link.name}</a>
+            </li>
+            {/each}
+        </ul>
+    </nav>
 </header>
 
-<style lang="scss"></style>
+<style lang="scss">
+    header {
+        border-bottom: 2px solid $text;
+
+        .logo {
+            width: 100%;
+            padding: $spacing-06;
+        }
+
+        nav {
+            padding: $spacing-04 $spacing-06;
+
+            border-top: 2px solid $text;
+
+            ul {
+                display: flex;
+                justify-content: space-between;
+            }
+        }
+
+    }
+</style>
