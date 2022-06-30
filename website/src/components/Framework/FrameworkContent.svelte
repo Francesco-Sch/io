5ea8e96958b97b0897d772888e057dc6b4931e82
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-06-30 12:06:44 +0200

    Sets headers for content modals

--- website/src/components/Framework/FrameworkContent.svelte
+++ website/src/components/Framework/FrameworkContent.svelte
@@ -2,6 +2,9 @@
 /* Store & Props */
 import { framework } from '../../store'
 export let index:number;
+let number = $framework[index].index
+let title = $framework[index].title
+let content = $framework[index].content
 let show = $framework[index].show
 
 /* Local variables */
@@ -15,19 +18,96 @@ show.subscribe(value => {
 
 {#if showContent}
     <div class="framework-content">
-        <p>Test</p>
+        <div class="header grid">
+            <div class="number-wrapper">
+                <div class="number">
+                    <p>{number}</p>
+                </div>
+            </div>
+            <h3>{title}</h3>
+            <div class="button-wrapper">
+                <button on:click={() => {
+                    show.set(false)
+                }}>
+                    <img src="/close-button.svg" alt="Close button">
+                </button>
+            </div>
+        </div>
     </div>
 {/if}
 
 <style lang="scss">
 .framework-content {
     position: fixed;
-    top: 95vh;
+    bottom: 0;
     left: 0;
+    z-index: 100;
 
     width: 100vw;
-    height: 5rem;
+    height: 85vh;
+
+    padding: $spacing-08 $spacing-06;
 
     background-color: $gray-10;
+
+    .header {
+        .number-wrapper {
+            grid-column: 1 / 2;
+            justify-self: start;
+            align-self: center;
+
+            .number {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 2.5rem;
+                height: 2.5rem;
+                border: 1px solid $text;
+                border-radius: 50%;
+                
+                p {
+                    text-indent: 0;
+                    font-family: $mono;
+                }
+                
+            }
+        }
+
+        h3 {
+            grid-column: 3 / 11;
+
+            font-size: $font-size-xxxl;
+            text-align: right;
+            text-transform: uppercase;
+        }
+
+        .button-wrapper {
+            grid-column: 11 / 12;
+            justify-self: end;
+            align-self: center;
+
+            button {
+                display: inline-flex;
+                justify-content: center;
+                align-items: center;
+                padding: $spacing-04;
+
+                border: 1px solid transparent;
+                border-radius: 0.2rem;
+
+                transition: all 0.3s $transition-ease;
+
+                &:hover {
+                    cursor: pointer;
+                    background-color: $gray-20;
+                }
+
+                &:active {
+                    cursor: pointer;
+                    background-color: $gray-40;
+                }
+            }
+        }
+    }
 }
 </style>
