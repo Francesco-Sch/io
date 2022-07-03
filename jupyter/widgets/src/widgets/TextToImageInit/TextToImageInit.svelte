5eb814251e0f42aae442ec09fd7bd707ce7c6d86
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-07-03 14:40:35 +0200

    Jupyter: Sort results of VQGAN generation

--- jupyter/widgets/src/widgets/TextToImageInit/TextToImageInit.svelte
+++ jupyter/widgets/src/widgets/TextToImageInit/TextToImageInit.svelte
@@ -24,9 +24,26 @@
 
         imagePaths.forEach(async (e:any) => {
             const imageRequest = await axios.get(`${window.location.origin}/api/contents/${e.path}`)
-            let image = await imageRequest.data.content
+            let image = {
+                created: '',
+                content: ''
+            }
+            image.content = await imageRequest.data.content
+            image.created = await imageRequest.data.created
 
             images.push(image)
+
+            images.forEach((e) => {
+                console.log("Before sort:" + e.created);
+            })
+
+            // Sort after dates
+            images.sort((a, b) => new Date(b.created) - new Date(a.created));
+
+            images.forEach((e) => {
+                console.log("After sort:" + e.created);
+            })
+
             images = images
         })
     }
@@ -50,8 +67,6 @@
     RUNNING STATE
     ---------------------------- */
     .tti-init :global(.running) {
-        width: 100%;
-        margin-top: 0;
         background-color: var(--orange-20);
     }
     .tti-init :global(.running .bx--inline-loading) {
@@ -78,9 +93,6 @@
     FINISHED STATE
     ---------------------------- */
     .tti-init :global(.finished) {
-        width: 100%;
-        margin-top: 0;
-
         font-weight: 500;
     }
     .tti-init :global(.finished:hover) {
@@ -91,31 +103,66 @@
 
         font-weight: 500;
     }
+
+    /* ----------------------------
+    IMAGE GRID
+    ---------------------------- */
+    .tti-init .images-wrapper {
+        margin-top: var(--spacing-06);
+        padding-top: var(--spacing-06);
+
+        border-top: 2px solid var(--gray-50);
+    }
+    .tti-init .images-grid {
+        display: flex;
+        flex-wrap: wrap;
+        max-height: 48rem;
+        overflow-y: auto;
+    }
+    .tti-init .images-grid :global(.single-image) {
+        width: 24.15% !important;
+        height: auto;
+        margin-right: var(--spacing-03);
+        margin-bottom: var(--spacing-03);
+
+        object-fit: contain;
+    }
 </style>
 
 <div class="io-widget tti-init">
-    {#if click == true && finished == false}
-        <div class="io_primary-btn running">
-            <InlineLoading description="Process is running..." />
-        </div>
-    {:else if finished == true}
-        <div class="io_primary-btn finished">
+    <div class="button-wrapper">
+        {#if click == true && finished == false}
+            <div class="io_primary-btn init-button running">
+                <InlineLoading description="Process is running..." />
+            </div>
+        {:else if finished == true}
+        <div class="io_primary-btn init-button finished">
             <CheckmarkFilled size={20} />
             <p>Scraper finished successfully</p>
         </div>
-    {:else}
-        <Button class="io_primary-btn init-button" on:click="{() => {
-            TextToImageInitClick.set(true);
-        }}">Start the generation process</Button>
-    {/if}
+        {:else}
+            <Button class="io_primary-btn init-button" on:click="{() => {
+                TextToImageInitClick.set(true);
+            }}">Start the generation process</Button>
+        {/if}
+    </div>
 
-    {#each images as image}
-        <ImageLoader 
-            src={`data:image/jpg;base64,${image}`}
-            alt="part of dataset" 
-            class="single-image"
-        />
-    {:else}
-        <p>Generate images should appear soon...</p>
-    {/each}
+    {#if click == true}
+        <div class="images-wrapper">
+            <h3 class="io_widget-headline">Generated results</h3>
+
+            
+            <div class="images-grid">
+                {#each images as image}
+                    <ImageLoader 
+                        src={`data:image/jpg;base64,${image.content}`}
+                        alt="part of dataset" 
+                        class="single-image"
+                    />
+                {:else}
+                    <p>Generate images should appear soon...</p>
+                {/each}
+            </div>
+        </div>
+    {/if}
 </div>
